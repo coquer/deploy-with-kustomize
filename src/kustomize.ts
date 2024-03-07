85cdd2e4a5c6adadfa9a8b12fef5e3130a0afcf2
Jorge Y. C. Rodriguez <self@jorgerodriguez.dk>
2024-03-07 12:23:27 +0100

    feat: init release open-source forked

--- src/kustomize.ts
+++ src/kustomize.ts
@@ -5,33 +5,33 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 const getKustomizeLink = (version: string): string => {
-    switch (os.type()) {
-        case 'Linux':
-            return `https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${version}/kustomize_v${version}_linux_amd64.tar.gz`
+  switch (os.type()) {
+    case 'Linux':
+      return `https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${version}/kustomize_v${version}_linux_amd64.tar.gz`
 
-        case 'Darwin':
-            return `https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${version}/kustomize_v${version}_darwin_amd64.tar.gz`;
-        case 'Windows_NT':
-            return `https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${version}/kustomize_v${version}_windows_amd64.tar.gz`;
-        default:
-            throw Error("Could not find correct OS")
-    }
+    case 'Darwin':
+      return `https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${version}/kustomize_v${version}_darwin_amd64.tar.gz`;
+    case 'Windows_NT':
+      return `https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${version}/kustomize_v${version}_windows_amd64.tar.gz`;
+    default:
+      throw Error("Could not find correct OS")
+  }
 }
 
 const getToolWithExtension = (toolName) => os.type() == 'Windows_NT' ? `${toolName}.exe` : toolName;
 
 export const setupKustomize = async (version) => {
-    const toolName = 'kustomize';
-    
-    let cachedToolpath = toolCache.find(toolName, version);
-    if (!cachedToolpath) {
-        const location = await toolCache.downloadTool(getKustomizeLink(version));
-        const unzipped = await toolCache.extractTar(location);
-        cachedToolpath = await toolCache.cacheDir(unzipped, toolName, version)
-    }
+  const toolName = 'kustomize';
 
-    const kustomizePath = path.join(cachedToolpath, getToolWithExtension(toolName));
-    fs.chmodSync(kustomizePath, '777');
+  let cachedToolpath = toolCache.find(toolName, version);
+  if (!cachedToolpath) {
+    const location = await toolCache.downloadTool(getKustomizeLink(version));
+    const unzipped = await toolCache.extractTar(location);
+    cachedToolpath = await toolCache.cacheDir(unzipped, toolName, version)
+  }
 
-    core.addPath(cachedToolpath)
+  const kustomizePath = path.join(cachedToolpath, getToolWithExtension(toolName));
+  fs.chmodSync(kustomizePath, '777');
+
+  core.addPath(cachedToolpath)
 }
